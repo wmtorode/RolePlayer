e49839c0608a290f45c8e5905134b992821b5513
Bill Torode <wmtorode@gmail.com>
2020-05-05 02:25:08 -0400

    init repo

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RolePlayer.Data;
+
+namespace RolePlayer
+{
+    public static class Main
+    {
+        internal static Logger modLog;
+    }
+}
