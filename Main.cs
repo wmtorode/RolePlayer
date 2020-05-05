46d968d9af302e6ba577111e7b61914bff624c56
Bill Torode <wmtorode@gmail.com>
2020-05-05 02:51:20 -0400

    just setting up the basics

--- Main.cs
+++ Main.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using RolePlayer.Data;
+using Harmony;
 
 namespace RolePlayer
 {
     public static class Main
     {
         internal static Logger modLog;
+
+        public static void Init(string modDir, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("ca.jwolf.RolePlayer");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            modLog = new Logger(modDir, "RolePlayer", true);
+
+        }
     }
 }
