beb9a86d55b65c4aabcf4773c8160d4b089b3fd1
Bill Torode <wmtorode@gmail.com>
2020-05-06 02:49:46 -0400

    a bit more work

---
+++ Data/BehaviorRoleDef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleTech;
+using Newtonsoft.Json;
+
+namespace RolePlayer.Data
+{
+    public class BehaviorRoleDef
+    {
+        public string roleTag;
+        public string behaviorFile;
+        public List<AIMood> moods;
+    }
+}
---
+++ Features/BehaviorVariableManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RolePlayer;
+using BattleTech;
+
+namespace RolePlayer.Features
+{
+    public class BehaviorVariableManager
+
+    {
+        private static BehaviorVariableManager instance;
+        private Dictionary<string, BehaviorVariableScope> scopesByRole;
+        private Dictionary<string, string> actorRoleCache;
+
+        public static BehaviorVariableManager Instance
+        {
+            get
+            {
+                if (instance == null) instance = new BehaviorVariableManager();
+                return instance;
+            }
+        }
+
+        public string behviorsDirectory { get; set; }
+
+
+    }
+
+
+}
